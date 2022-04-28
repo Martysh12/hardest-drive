10b8f15769ddce92c3af8a41581c8d81e8c17391
Martysh12 <49569238+Martysh12@users.noreply.github.com>
2022-04-28 15:16:24 +0300

    Add make_hexdump function
    it makes hexdumps

--- bot.py
+++ bot.py
@@ -9,6 +9,27 @@ from constants import *
 
 load_dotenv()
 
+# FUNCTIONS #
+
+def make_hexdump(data, page, bytes_per_line=16, offset=0x0000): 
+    # Chunk generator
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+    print(" " * 7 + " ".join([f"{i:02X}" for i in range(bytes_per_line)]))
+    
+    counter = offset
+    for i in chunks(data, bytes_per_line):
+        print(f"{counter:04X} :",
+            binascii.hexlify(i, " ").decode("UTF-8").ljust(bytes_per_line * 3)
+            + "".join([chr(j) if chr(j).isascii() and chr(j).isprintable() else "." for j in i]))
+
+        counter += bytes_per_line
+
+#############
+
 print(f"HardestDrive v1.0 by Martysh12#1610")
 
 bot = commands.Bot(command_prefix=PREFIX, activity=nextcord.Game(PREFIX + "help"))
